7971e56ff66b37110de230c37f91d10c9f767f1b
everestclimber <everestclimber89@gmail.com>
2022-08-16 13:50:03 -0400

    add user query param

--- web/src/components/Video/Videos/Videos.tsx
+++ web/src/components/Video/Videos/Videos.tsx
@@ -1,18 +1,23 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Link, routes, useLocation } from '@redwoodjs/router'
 
 import VideoThumbnail from 'src/components/VideoThumbnail'
 
 const VideosList = ({ videos }) => {
+  const location = useLocation()
+  const userId = location.search.split('?userId=')?.[1]
+
   return (
     <div className="flex flex-wrap gap-4">
-      {videos.map((video) => (
-        <div key={video.id}>
-          <Link to={routes.video({ id: video.id })}>
-            <VideoThumbnail video={video} />
-            <div>{video.title}</div>
-          </Link>
-        </div>
-      ))}
+      {videos
+        .filter((video) => (userId ? video.user.id === parseInt(userId) : true))
+        .map((video) => (
+          <div key={video.id}>
+            <Link to={routes.video({ id: video.id })}>
+              <VideoThumbnail video={video} />
+              <div>{video.title}</div>
+            </Link>
+          </div>
+        ))}
     </div>
   )
 }
